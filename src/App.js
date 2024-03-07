4e30ae886daf2c4a11045a4a6874268f4f59f92c
soaelee <soae0923@gmail.com>
2024-03-07 21:06:42 +0900

    styled-components: theme only 50%

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import styled, { keyframes } from "styled-components";
 
-// Pseudo Selectors part Two
+// Themes
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 function App() {
   return (
-    <Father>
-      <Box>
-        <Emoji>😃</Emoji>
-      </Box>
-      <Emoji>😃</Emoji>
-    </Father>
+    <Wrapper>
+      <Title>Title</Title>
+    </Wrapper>
   );
 }
 
