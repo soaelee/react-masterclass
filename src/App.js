f2e71fe83c5f50f5c5b5d1aba3f5dd22c9478644
soaelee <soae0923@gmail.com>
2024-03-07 20:48:04 +0900

    styled-components: Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-// 'As' and Attrs
+// Animations and Pseudo Selectors
 
 const Father = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 function App() {
   return (
     <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Btn as="a" href="/">
-        Login
-      </Btn>
+      <Box>
+        <span>😃</span>
+      </Box>
     </Father>
   );
 }
