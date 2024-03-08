189758f678d98f619a6aa518b34e14098ac4ee1b
soaelee <soae0923@gmail.com>
2024-03-09 05:13:44 +0900

    typescript: Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from "react";
+import styled from "styled-components";
 
-// Forms
+// Themes
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(`hello ${value}`);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 
